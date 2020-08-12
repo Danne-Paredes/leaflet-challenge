2ea35712d7f27c549d991d8df7ac65b144a7413c
Danne Paredes <danneparedes@gmail.com>
2020-08-11 22:33:25 -0700

    cleaned up unneeded files

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,31 +1,28 @@
-// How to capitalize first letter easily? is something like this really the easiest way?:
+// How to capitalize first letter easily? is something like this really the easiest way?(this is for the marker pop-ups, i waned to capitalize the properties.type):
     // https://stackoverflow.com/questions/32589197/how-can-i-capitalize-the-first-letter-of-each-word-in-a-string-using-javascript
-// How to mak markers circles
-// How to change size/color of markers based on magnitude
-// how to add legend
+
+// Please check line 119 for a question
 
 
 function chooseColor(magnitude) {
   switch (true) {
   case magnitude > 5:
-    return "red";
+    return "#E74737";
   case magnitude > 4:
-    return "orange";
+    return "#F49D2C";
   case magnitude > 3:
-    return "orange";
-  case "Queens":
-    return "green";
+    return "#E7BB4A";
   case magnitude > 2:
-    return "purple";
+    return "#2C8983";
   case magnitude > 1:
-  return "purple";
+  return "#0E3C74";
   default:
-    return "green";
+    return "#0000FF";
   }
 }
 
 
-function createMap(earthquakes) {
+function createMap() {
 
   // Create the tile layer that will be the background of our map
   var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -50,6 +47,12 @@ function createMap(earthquakes) {
     accessToken: API_KEY
   });
 
+  var earth = new L.LayerGroup()
+  var overlays = {
+    Earthquakes: earth
+  }
+
+
   // Create a baseMaps object to hold the lightmap layer
   var baseMaps = {
     "Street Map": streetmap,
@@ -57,123 +60,78 @@ function createMap(earthquakes) {
     "Light Map": lightmap
   };
 
-  // Create an overlayMaps object to hold the earthquakes layer
-  var overlayMaps = {
-    "Earthquakes": earthquakes
-  };
-
   // Create the map object with options
   var map = L.map("map", {
     center: [37.7749, -100.4194],
     zoom: 3,
-    layers: [streetmap, earthquakes]
+    layers: [streetmap,earth]
   });
 
   // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(map);
-}
+  L.control.layers(baseMaps,overlays).addTo(map);
 
-function createMarkers(response) {
+  // Grabbing our GeoJSON data..
+  d3.json(url, function(data) {
+    // Creating a geoJSON layer with the retrieved data
+    L.geoJson(data, {
+      pointToLayer:function(feature,latlng){
+        return L.circleMarker(latlng)
+      },
+      // Style each feature (in this case a neighborhood)
+      style: function(feature) {
+        return {
+          color: "#000",
+          // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
+          fillColor: chooseColor(feature.properties.mag),
+          fillOpacity: 1,
+          weight: 1.5,
+          radius: feature.properties.mag*2
+        };
+      },
+      // Called on each feature
+      onEachFeature: function(feature, layer) {
+        // Giving each feature a pop-up with information pertinent to it
+        layer.bindPopup(`<h3>${feature.properties.type}: ${feature.properties.title}<h3><hr><p>${new Date(feature.properties.time)}</p>`);
+  
+      }
+    }).addTo(earth);
 
-  // Pull the "quakes" property off of response.data
-  var quakes = response.features
-  console.log(quakes)
-
-  // Initialize an array to hold bike markers
-  var earthquakes = [];
-
-  // Loop through the quakes array
-  for (var index = 0; index < quakes.length; index++) {
-    var quake = quakes[index].geometry;
-
-    // For each station, create a marker and bind a popup with the station's name
-    var quakeMarkers = L.circleMarker([quake.coordinates[1], quake.coordinates[0]])
-      .bindPopup(`<h3>${quakes[index].properties.type}: ${quakes[index].properties.title}<h3><hr><p>${new Date(quakes[index].properties.time)}</p>`);
-
-    // Add the marker to the earthquakes array
-    earthquakes.push(quakeMarkers);
-  }
-
-  // Create a layer group made from the bike markers array, pass it into the createMap function
-  createMap(L.layerGroup(earthquakes));
-
-  // Set up the legend
-  var legend = L.control({ position: "bottomright" });
-  legend.onAdd = function() {
-    var div = L.DomUtil.create("div", "info legend");
-    var limits = geojson.options.limits;
-    var colors = geojson.options.colors;
-    var labels = [];
-
-    // Add min & max
-    var legendInfo = "<h1>Median Income</h1>" +
-      "<div class=\"labels\">" +
-        "<div class=\"min\">" + limits[0] + "</div>" +
-        "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-      "</div>";
-
-    div.innerHTML = legendInfo;
-
-    limits.forEach(function(limit, index) {
-      labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+    // an object legend
+    var legend = L.control({
+      position: "bottomright"
     });
+  
+    // details for the legend
+    legend.onAdd = function() {
+      var div = L.DomUtil.create("div", "info legend");
+  
+      var grades = [0, 1, 2, 3, 4, 5];
+      var colors = [
+        "#0000FF",
+        "#0E3C74",
+        "#2C8983",
+        "#E7BB4A",
+        "#F49D2C",
+        "#E74737"
+      ];
+  
+  
+      // Looping through, SIDE NOTE: line 123 I wasn't sure how to make the ternary operator fully work with the f-string so I had to use the concatenation for that part. Can you show how to do it correctly?
+      for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+          `<i style='background:${colors[i]}'></i>
+          ${grades[i]} ${(grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+")}`;
+      }
+      return div;
+    };
+  
+    // Finally, we our legend to the map.
+    legend.addTo(map);
 
-    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-    return div;
-  };
-
-  // Adding legend to the map
-  legend.addTo(myMap);
+  });
+  
 }
-var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson";
 
-// Perform an API call to the Citi Bike API to get quake information. Call createMarkers when complete
-d3.json(url, createMarkers);
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 
-
-
-// Grabbing our GeoJSON data..
-d3.json(link, function(data) {
-  // Creating a geoJSON layer with the retrieved data
-  L.geoJson(data, {
-    // Style each feature (in this case a neighborhood)
-    style: function(feature) {
-      return {
-        color: "white",
-        // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
-        fillColor: chooseColor(feature.properties.borough),
-        fillOpacity: 0.5,
-        weight: 1.5
-      };
-    },
-    // Called on each feature
-    onEachFeature: function(feature, layer) {
-      // Set mouse events to change map styling
-      layer.on({
-        // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
-        mouseover: function(event) {
-          layer = event.target;
-          layer.setStyle({
-            fillOpacity: 0.9
-          });
-        },
-        // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
-        mouseout: function(event) {
-          layer = event.target;
-          layer.setStyle({
-            fillOpacity: 0.5
-          });
-        },
-        // When a feature (neighborhood) is clicked, it is enlarged to fit the screen
-        click: function(event) {
-          myMap.fitBounds(event.target.getBounds());
-        }
-      });
-      // Giving each feature a pop-up with information pertinent to it
-      layer.bindPopup("<h1>" + feature.properties.neighborhood + "</h1> <hr> <h2>" + feature.properties.borough + "</h2>");
-
-    }
-  }).addTo(myMap);
-});
+createMap()
